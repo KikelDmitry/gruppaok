14661906cf82c0881f68f96db6032a29fd958560
Kikel Dmitry <kikeldmitry@gmail.com>
2021-01-17 00:29:02 +0300

    work in progress

---
+++ gulpfile.js
@@ -0,0 +1,213 @@
+'use strict';
+
+//PCKGS
+const { src, dest, parallel, series, watch } = require('gulp');
+
+//common
+const browserSync = require('browser-sync');
+const concat = require('gulp-concat');
+const sourcemaps = require('gulp-sourcemaps');
+const del = require('del');
+
+//html
+const gulpPug = require('gulp-pug');
+
+// css
+const gulpSass = require('gulp-sass');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const csso = require('postcss-csso');
+
+//js
+const minify = require('gulp-minify');
+
+//bitmap
+const imagemin = require('gulp-imagemin');
+
+//svg
+const gulpSvgSprite = require('gulp-svg-sprite');
+const cheerio = require('gulp-cheerio');
+const svgmin = require('gulp-svgmin');
+
+
+//CONFIG
+
+const config = {
+	src: './src/',
+	dest: './build/',
+};
+const globs = {
+	pug: [
+		config.src + 'pug/**/*.pug',
+		'!' + config.src + 'pug/**/_*/*.pug',
+	],
+	scss: config.src + 'scss/main.scss',
+	js: [
+		'node_modules/@glidejs/glide/dist/glide.min.js',
+		config.src + 'js/**/*.js',
+	],
+	images: [
+		config.src + 'img/**/*.{png,jpg,jpeg,svg,gif}',
+		'!' + config.src + 'img/sprite/**/*.svg'
+	],
+	sprite: config.src + 'img/sprite/**/*.svg',
+	fonts: config.src + 'fonts/**/*.*',
+}
+
+//server
+const server = () => {
+	browserSync.init({
+		server: {
+			baseDir: config.dest
+		}
+	})
+};
+
+//TASKS
+const pug = () => {
+	return src(globs.pug)
+		.pipe(gulpPug({
+			pretty: false,
+			basedir: './'
+		}))
+		.pipe(dest(config.dest))
+		.pipe(browserSync.stream())
+};
+exports.pug = pug;
+
+const scss = () => {
+	return src(globs.scss)
+		.pipe(sourcemaps.init())
+		.pipe(gulpSass({
+			outputStyle: 'expanded'
+		}))
+		.on('error', gulpSass.logError)
+		.pipe(postcss([
+			autoprefixer(),
+			csso()
+		]))
+		.pipe(sourcemaps.write('.'))
+		.pipe(dest(config.dest + 'css'))
+		.pipe(browserSync.stream())
+
+};
+
+const criticalCss = () => {
+	return src(globs.criticalScss)
+		.pipe(gulpSass({
+
+		}))
+		.pipe(postcss([
+			autoprefixer(),
+			csso(),
+		]))
+		.pipe(dest(config.dest + 'css'))
+		.pipe(browserSync.stream())
+};
+
+const scripts = () => {
+	return src(globs.js)
+		.pipe(concat('bundle.js'))
+		.pipe(minify({
+			ext: {
+				min: '.min.js'
+			}
+		}))
+		.pipe(dest(config.dest + 'js'))
+		.pipe(browserSync.stream())
+};
+
+const images = () => {
+	return src(globs.images)
+		.pipe(imagemin({
+			plugins: [
+				imagemin.gifsicle({ interlaced: true }),
+				imagemin.mozjpeg({ quality: 75, progressive: true }),
+				imagemin.optipng({ optimizationLevel: 5 }),
+				imagemin.svgo({
+					plugins: [
+						{ removeViewBox: true },
+						{ cleanupIDs: false }
+					]
+				})
+
+			],
+			verbose: true
+		}))
+		.pipe(dest(config.dest + 'img'))
+};
+exports.images = images;
+
+
+const svgsprite = () => {
+	return src(globs.sprite)
+		.pipe(svgmin({
+			js2svg: {
+				pretty: true
+			}
+		}))
+		.pipe(cheerio({
+			run: function ($) {
+				$('[fill]').removeAttr('fill');
+				$('[stroke]').removeAttr('stroke');
+				$('[style]').removeAttr('style');
+			},
+			parserOptions: { xmlMode: true }
+		}))
+		.pipe(gulpSvgSprite({
+			mode: {
+				symbol: {
+					sprite: '../sprite.svg'  //sprite file name
+				}
+			},
+		}
+		))
+		.pipe(dest(config.dest + 'img'));
+};
+exports.svgsprite = svgsprite;
+
+const fonts = () => {
+	return src(globs.fonts)
+		.pipe(dest(config.dest + 'fonts'))
+};
+
+const watcher = () => {
+	watch(config.src + 'pug/**/*.pug', pug)
+	watch(config.src + 'scss/**/*.scss', scss)
+	// watch(config.src + 'scss/**/critical.scss', series(criticalCss, pug))
+	watch(globs.js, scripts)
+	watch(globs.images, images)
+	watch(globs.sprite, svgsprite)
+};
+
+
+const clean = () => {
+	return del(config.dest + '**', { force: true })
+};
+exports.clean = clean;
+
+
+//build task
+exports.build = series(
+	clean,
+	parallel(
+		series(
+			scss,
+			// criticalCss,
+			pug,
+		),
+		scripts,
+		svgsprite,
+		images,
+		fonts
+	)
+);
+
+//development task
+exports.dev = series(
+	this.build,
+	parallel(
+		server,
+		watcher
+	)
+);
---
+++ src/js/main.js
@@ -0,0 +1,116 @@
+// modal layer
+const mLayer = document.querySelector('.modal-layer'),
+	html = document.querySelector('html'),
+	body = document.querySelector('body');
+function mLayerOn() {
+	mLayer.classList.add('is-active');
+	mLayer.style.visibility = 'visible';
+	html.classList.add('modal-open');
+	body.classList.add('modal-open');
+}
+function mLayerOff() {
+	mLayer.classList.remove('is-active');
+	mLayer.style.visibility = 'hidden';
+	html.classList.remove('modal-open');
+	body.classList.remove('modal-open');
+}
+
+mLayer.addEventListener('click', function (e) {
+	if (e.target == this) {
+		mClose();
+	}
+});
+
+function mClose() {
+	mLayerOff();
+	modalWin.classList.remove('is-active');
+	modalWin.setAttribute('aria-hidden', 'true');
+	menuWrap.classList.remove('modal-open');
+}
+
+// menu open
+const burger = document.querySelector('.burger_btn'),
+	menuWrap = document.querySelector('.top-menu'),
+	menu = document.querySelector('.top-menu__nav'),
+	menuLink = document.querySelectorAll('.top-menu__link');
+function menuOn() {
+	burger.classList.add('is-active');
+	menu.classList.add('is-active');
+	menuWrap.classList.add('is-active');
+	menuWrap.setAttribute('aria-hidden', 'false'); 
+	mLayerOn();
+}
+function menuOff() {
+	burger.classList.remove('is-active');
+	menu.classList.remove('is-active');
+	menuWrap.classList.remove('is-active');
+	menuWrap.setAttribute('aria-hidden', 'true');
+	mLayerOff();
+}
+burger.addEventListener('click', function () {
+	if (burger.classList.contains('is-active')) {
+		menuOff();
+	} else {
+		menuOn();
+	}
+});
+menuLink.forEach(function (item) {
+	item.addEventListener('click', menuOff);
+});
+
+//brands slider
+new Glide('.brands__glide', {
+	type: 'carousel',
+	perView: '7',
+	gap: 20,
+	autoplay: true,
+	animationDuration: 2000,
+	animationTimingFunc: 'linear',
+	breakpoints: {
+		1024: {
+			perView: 6
+		},
+		768: {
+			perView: 5
+		},
+		640: {
+			perView: 4,
+			gap: 10
+		},
+		480: {
+			perView: 3
+		},
+		360: {
+			perView: 2
+		}
+	}
+}).mount();
+
+new Glide('.photo__slider', {
+	type: 'carousel',
+	width: 500
+}).mount();
+
+//totop
+const topBtn = document.querySelectorAll('.js-top');
+topBtn.forEach(function (butElem) {
+	butElem.addEventListener('click', toTop);
+});
+function toTop() { //smooth scroll
+	const scrollStep = document.body.scrollHeight / 100;
+	if (window.pageYOffset > 0) {
+		window.scrollBy(0, -(scrollStep));
+		setTimeout(toTop, 0);
+	} else {
+		return false
+	}
+};
+function btnAppear() {
+	const topMenu = document.querySelector('.top-menu');
+	if (window.pageYOffset > window.innerHeight) {
+		topMenu.classList.add('go-down');
+	} else {
+		topMenu.classList.remove('go-down');
+	}
+};
+window.addEventListener('scroll', btnAppear);
